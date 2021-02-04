41370b68e6ab6a047fdc3e8b2c25615ea3d70560
TheCapar <deha capar@DESKTOP-IBU3JE1.(none)>
2021-02-04 20:38:05 +0300

    Proje dosyası ekle.

---
+++ Dictonary/MyDictonary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictonary<T>
+    {
+        T[] items;
+        public MyDictonary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArrays = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArrays.Length; i++)
+            {
+                items[i] = tempArrays[i];
+            }
+            items[items.Length - 1] = item;
+        }
+        public int Lenght
+        {
+            get { return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ Dictonary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<string> isim = new MyDictonary<string>();
+            isim.Add("Deha");
+            Console.WriteLine(isim.Lenght);
+            isim.Add("Nazım");
+            Console.WriteLine(isim.Lenght);
+            foreach (var isimler in isim.Items)
+            {
+                Console.WriteLine(isimler);
+            }
+        }
+	
+
+	
+    }
+}
